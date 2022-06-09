932081084c3cc38ffca47a7302229659bb15512a
Joao Pedro Andrade Moreira <64596663+JoaoPedroAM@users.noreply.github.com>
2022-06-09 00:31:50 -0300

    Adicionando database

--- Projeto de c206-e-bd/src/main/java/pokemon/Pokemon.java
+++ Projeto de c206-e-bd/src/main/java/pokemon/Pokemon.java
@@ -4,17 +4,30 @@ import lombok.Data;
 
 @Data
 public abstract class Pokemon {
+
+
+    protected int id;
     protected String nome;
     protected String tipo;
-    protected double atk;
-    protected double def;
+    protected int atk;
+    protected int def;
     protected int vida;
     protected int lvl;
     protected boolean shiny;
+    protected boolean possui;
 
 
-
-
+    public Pokemon(int id, String nome, String tipo, int atk, int def, int vida, int lvl, boolean shiny, boolean possui) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.atk = atk;
+        this.def = def;
+        this.vida = vida;
+        this.lvl = lvl;
+        this.shiny = shiny;
+        this.possui = possui;
+    }
 
     private void capturar(){
 
