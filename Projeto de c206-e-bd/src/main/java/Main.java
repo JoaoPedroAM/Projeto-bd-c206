3b79579334e0acca7928f955fbf51da5bde6217c
LucasFranqueiraInatel <lucas.f@gec.inatel.br>
2022-06-16 22:00:00 -0300

    Mudancas

--- Projeto de c206-e-bd/src/main/java/Main.java
+++ Projeto de c206-e-bd/src/main/java/Main.java
@@ -2,6 +2,7 @@ import Database.LocalDB;
 import Database.PokemonDB;
 import Database.PokedexDB;
 import pokemon.Pokemon;
+import pokemon.PokemonPossuido;
 import pokemon.lugares.Local;
 import java.util.Random;
 import java.util.Scanner;
@@ -9,55 +10,132 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
-
         LocalDB localDB = new LocalDB();
         PokemonDB pokemonDB = new PokemonDB();
         PokedexDB pokedexDB = new PokedexDB();
 
-        pokemonDB.buscarSeusPokemons();
-//        int save;
-//
-//        Scanner sc = new Scanner(System.in);
-//
-//
-//        boolean flagSave = true;
-//        System.out.println("BOM DIA caçador de pokemons !!!");
-//        System.out.println("Pronto para completar a sua Pokedex ???");
-//        System.out.println("");
-//        System.out.println("Selecione o save: ");
-//        System.out.println("");
-//
-//        while (flagSave){
-//            pokedexDB.buscarPokedex();
-//            int auxSave;
-//            System.out.println("------>");
-//            save = sc.nextInt();
-//            if (save >= 1 && save <= 3){
-//                break;
-//            }
-//            else {
-//                System.out.println("Save invalido, tente novamante");
-//            }
-//
-//            boolean flagMenu = true;
-//            while(flagMenu){
-//                int auxMenu;
-//                System.out.println("1 - Mostrar seus pokemons:");
-//                System.out.println("2 - Escolher local de captura de pokemons:");
-//                System.out.println("3 - Escolher outro save");
-//                System.out.println("------>");
-//                auxMenu = sc.nextInt();
-//
-//                switch (auxMenu){
-//                    case 1:
-//                        pokemonDB.buscarSeusPokemons();
-//                }
-//            }
-//
-//        }
+        int save;
 
 
+        Scanner sc = new Scanner(System.in);
+
+
+        boolean flagSave = true;
+        System.out.println("BOM DIA caçador de pokemons !!!");
+        System.out.println("Pronto para completar a sua Pokedex ???");
+        System.out.println("");
+        System.out.println("Selecione o save: ");
+        System.out.println("");
+
+        while (flagSave) {
+            while (true) {
+                pokedexDB.buscarPokedex();
+                int auxSave;
+                System.out.println("------>");
+                save = sc.nextInt();
+                if (save >= 1 && save <= 3) {
+                    break;
+                } else {
+                    System.out.println("Save invalido, tente novamante");
+                }
+            }
+
+            boolean flagMenu = true;
+            while (flagMenu) {
+                int auxMenu;
+                System.out.println("1 - Mostrar seus pokemons:");
+                System.out.println("2 - Escolher local de captura de pokemons:");
+                System.out.println("3 - Escolher outro save");
+                System.out.println("------>");
+                auxMenu = sc.nextInt();
+
+                switch (auxMenu) {
+                    case 1:
+                        pokedexDB.buscarSeusPokemons(save);
+                        break;
+                    case 2:
+                        boolean flagLocal = true;
+                        while (flagLocal) {
+                            System.out.println("Escolha sua cidade: ");
+                            int local;
+                            int task;
+                            System.out.println("1 - Cidade Inicial <Pallet>");
+                            System.out.println("2 - Cidade Aquatica <Vermilion>");
+                            System.out.println("3 - Cidade Mal Assombrada <Lavender>");
+                            System.out.println("4 - Voltar");
+                            System.out.println("------>");
+                            local = sc.nextInt();
+                            int auxCacar = 0;
+                            switch (local) {
+                                case 1:
+                                    System.out.println("1 - Cacar pokemon nas redondezas");
+                                    System.out.println("2 - Informacoes do local");
+                                    System.out.println("≠ - Voltar");
+                                    System.out.println("------>");
+                                    auxCacar = sc.nextInt();
+                                    switch (auxCacar) {
+                                        case 1:
+                                            cacar(auxCacar, save, local,2,1);
+                                            break;
+                                        case 2:
+                                            System.out.println("Cidade de Pallet é uma pequena cidade localizada à sudoeste na Região de Kanto");
+                                            System.out.println("Seus pokemons sao:");
+                                            localDB.buscarPokemonCidade(1);
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                    break;
+                                case 2:
+                                    System.out.println("1 - Cacar pokemon nas redondezas");
+                                    System.out.println("2 - Informacoes do local");
+                                    System.out.println("3 - Voltar");
+                                    System.out.println("------>");
+                                    auxCacar = sc.nextInt();
+                                    switch (auxCacar) {
+                                        case 1:
+                                            cacar(auxCacar, save, local,4,3);
+                                            break;
+                                        case 2:
+                                            System.out.println("Vermilion City é uma cidade portuária localizada ao sul de Saffron e localizada na parte central de Kanto");
+                                            System.out.println("Seus pokemons sao:");
+                                            localDB.buscarPokemonCidade(2);
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                    break;
+                                case 3:
+                                    System.out.println("1 - Cacar pokemon nas redondezas");
+                                    System.out.println("2 - Informacoes do local");
+                                    System.out.println("3 - Voltar");
+                                    System.out.println("------>");
+                                    auxCacar = sc.nextInt();
+                                    switch (auxCacar) {
+                                        case 1:
+                                            cacar(auxCacar, save, local,6,5);
+                                            break;
+                                        case 2:
+                                            System.out.println("Lavender Town é localizada na parte Leste de Kanto e conhecida por causa da Pokémon Tower e seu cemiterio");
+                                            System.out.println("Seus pokemons sao:");
+                                            localDB.buscarPokemonCidade(3);
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                    break;
+                                case 4:
+                                    flagLocal = false;
+                                    break;
+                                default:
+                                    System.out.println("Opcao invalida, tente novamente: ");
+                                    break;
+                            }
+                        }
+                }
+            }
+
+        }
 
 //        Pokemon p1 = new Pokemon(1,"Bulbasaur","Grama",2);
 //        Pokemon p2 = new Pokemon(2,"Ivysaur","Grama",2);
@@ -75,17 +153,9 @@ public class Main {
 //        pokemonDB.criarPokemon(p6);
 
 
-
-
-
-
-
-
-
 //        Local n = new Local(4,"ali");
 //        localDB.insertCidade(n);
 //        localDB.atualizarCidade(4,"tete");
-
 
 
 //        Pokemon bulbasaur = new Pokemon(1,"Bulbasaur","Grama",5,false,true);
@@ -93,8 +163,6 @@ public class Main {
 
 //        Pokemon bulbasaur = new Pokemon(1,"Bulbasaur","Grama",1);
 //        Pokemon ivysaur = new Pokemon(2,"Ivysaur","Grama",1);
-
-
 
 
 //        Database database = new Database();
@@ -112,6 +180,22 @@ public class Main {
 //
 //        database.researchPokemonPossui(true);
 
+
+    }
+
+    public static void cacar(int auxCacar, int save, int local, int max, int min) {
+
+        LocalDB localDB = new LocalDB();
+        PokemonDB pokemonDB = new PokemonDB();
+        PokedexDB pokedexDB = new PokedexDB();
+
+        int idPok;
+        int lvlPok;
+        boolean shinyPok;
+        idPok = Local.gerarpokemon(max, min);
+        lvlPok = Local.gerarLvl();
+        shinyPok = Local.gerarShiny();
+        pokedexDB.adcionarPokemon(idPok, lvlPok, shinyPok, save, local);
 
     }
 }
---
+++ Projeto de c206-e-bd/src/main/java/pokemon/PokemonPossuido.java
@@ -0,0 +1,46 @@
+package pokemon;
+
+public class PokemonPossuido extends Pokemon {
+
+    private int lvl;
+    private boolean shiny;
+    private int minhaPokedex;
+
+    public PokemonPossuido(int id, String nome, String tipo, int local_idlocal, int minhaPokedex, int atoa1, int atoa2, int lvl, boolean shiny) {
+        super(id, nome, tipo, local_idlocal);
+        this.lvl = lvl;
+        this.shiny = shiny;
+        this.minhaPokedex = minhaPokedex;
+    }
+
+    public PokemonPossuido(int id, String nome, String tipo, int lvl, boolean shiny) {
+        super(id, nome, tipo);
+        this.lvl = lvl;
+        this.shiny = shiny;
+    }
+
+    public int getLvl() {
+        return lvl;
+    }
+
+    public void setLvl(int lvl) {
+        this.lvl = lvl;
+    }
+
+    public boolean isShiny() {
+        return shiny;
+    }
+
+    public void setShiny(boolean shiny) {
+        this.shiny = shiny;
+    }
+
+    public int getMinhaPokedex() {
+        return minhaPokedex;
+    }
+
+    public void setMinhaPokedex(int minhaPokedex) {
+        this.minhaPokedex = minhaPokedex;
+    }
+}
+
